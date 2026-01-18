d02eb4adaf2bff49b9662d64b3b81fbe31b644b5
hulehe <huzhenkai@naver.com>
2026-01-18 22:14:05 +0900

    updated

--- src/gz_world/launch/create_world.launch.py
+++ src/gz_world/launch/create_world.launch.py
@@ -17,29 +17,42 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    SetEnvironmentVariable,
+    IncludeLaunchDescription,
+)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    pkg_gz_world = get_package_share_directory("gz_world")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    set_env = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=os.path.abspath(os.path.join(pkg_gz_world, '..')), 
+    )
+    # print(f"Resource path: {pkg_gz_world}")
+    # print(f"Resource path: {pkg_ros_gz_sim}")
+
+    
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        # launch_arguments={
-        #    'gz_args': '-r gpu_lidar.sdf'
-        # }.items(),
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={
+            "gz_args": f'-r {os.path.join(pkg_gz_world, "worlds", "default.sdf")}'
+        }.items(),
     )
 
     ld = LaunchDescription()
 
+    ld.add_action(set_env)
     ld.add_action(gz_sim)
 
     return ld
