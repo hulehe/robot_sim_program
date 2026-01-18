77079197059f589069308b65d5c4a30bc7e4a570
hulehe <huzhenkai@naver.com>
2026-01-18 22:50:02 +0900

    添加turtlebot3_description

---
+++ src/turtlebot3_description/launch/turtlebot3_burger_state_publisher.launch.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PythonExpression
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    dec_use_sim_time = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    urdf = os.path.join(
+        get_package_share_directory("turtlebot3_description"),
+        "urdf",
+        "turtlebot3_burger.urdf",
+    )
+
+    robot_desc = Command(["xacro ", urdf])
+
+    rsp_params = {"robot_description": robot_desc}
+
+    robot_state_publisher = Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[rsp_params, {"use_sim_time": use_sim_time}],
+            )
+
+    ld = LaunchDescription()
+
+    ld.add_action(dec_use_sim_time)
+
+    ld.add_action(robot_state_publisher)
+
+    return ld
